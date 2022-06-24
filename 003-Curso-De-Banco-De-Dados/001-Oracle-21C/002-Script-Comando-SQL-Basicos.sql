6b9435cdc9845b37b7ff15cbb74e05e923d7eba3
Adriano Vilela <98471300+Adrianogvs@users.noreply.github.com>
2022-06-24 09:51:57 -0300

    Meus Projetos De Estudos.

---
+++ 003-Curso-De-Banco-De-Dados/001-Oracle-21C/002-Script-Comando-SQL-Basicos.sql
@@ -0,0 +1,20 @@
+-- COMANDOS SQL BASICOS
+
+/*
+ 
+SELECT DISTINCT {*, COLUNA [ALIAS], ...}
+FROM TABLE
+WHERE COLUNA;
+
+
+  SELECT     : E uma lista de uma ou mais colunas.
+  DISTINCT   : Suprime duplicidades
+  *          : Seleciona todas as colunas
+  colunm     : Seleciona a coluna nomeada
+  alias      : Fornece para as colunas selecionadas titulos diferentes.
+  FROM table : Especifica a tabela que contém as colunas
+  WHERE      : Fazer uma busca em determinada coluna
+
+ */
+
+
